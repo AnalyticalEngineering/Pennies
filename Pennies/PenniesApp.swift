5f0c9e9ecbe7e919414dd5c5369607a953669539
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-29 10:29:30 -0600

    Initial Commit

---
+++ Pennies/PenniesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenniesApp.swift
+//  Pennies
+//
+//  Created by J. DeWeese on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PenniesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
